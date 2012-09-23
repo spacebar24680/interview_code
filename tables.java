58bdfa57c678189b44281b68c5c097a0c492ae6d
unknown <alexander.aur@gmail.com>
2012-09-22 21:04:02 -0500

    Modified tables to output without leading spaces

--- tables.java
+++ tables.java
@@ -3,7 +3,10 @@ public class tables {
 	public static void main(String [] args){
 		for(int a = 1; a <= 3; a++){
 			for(int b = 1; b <= 12; b++){
-				System.out.format("%4d", a*b);
+				if(b == 1)
+					System.out.print(a*b);
+				else
+					System.out.format("%4d", a*b);
 			}
 			System.out.println();
 		}
