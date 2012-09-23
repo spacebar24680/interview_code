afd9f211c2c7e12966a9fe1f879735f3097dc293
unknown <alexander.aur@gmail.com>
2012-09-22 21:06:38 -0500

    12x12 multiplcation table for tables.java

--- tables.java
+++ tables.java
@@ -1,13 +1,12 @@
 
 public class tables {
 	public static void main(String [] args){
-		for(int a = 1; a <= 3; a++){
+		for(int a = 1; a <= 12; a++){
 			for(int b = 1; b <= 12; b++){
-				if(b == 1)
-					System.out.print(a*b);
-				else
+
 					System.out.format("%4d", a*b);
 			}
+			if(a + 1 <= 12)
 			System.out.println();
 		}
 	}
